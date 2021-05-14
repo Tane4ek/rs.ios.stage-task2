e308595c2d839631e96ff82f46a7ead7b9a4bb48
Tatyana Luzanova <82838416+Tane4ek@users.noreply.github.com>
2021-05-15 00:28:34 +0500

    docs: add solution of exercises 1,2.

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,25 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    
+    int resuide;
+    NSMutableString *myString = [[NSMutableString alloc] init];
+    while (n != 0) {
+        resuide = n % 2;
+        n = (n - resuide)/ 2;
+        NSString *ch = [@(resuide) stringValue];
+        [myString appendString:ch];
+    }
+    while (myString.length !=8){
+        [myString appendString:@"0"];
+        
+    }
+    UInt8 reversInteger = 0;
+    for (int i = 0; i < myString.length; ++i){
+        char character = [myString characterAtIndex:i];
+        if (character == '1') {
+        reversInteger += pow(2, (myString.length - 1 - i));
+        }
+    }
+    return reversInteger;
 }
--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,25 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    
+    int count = 0;
+    for(int i = 0 ; i < [array count] - 1; i++) {
+        NSNumber *value1 = [array objectAtIndex:i];
+        
+        for (int j = i+1; j < [array count]; j++) {
+            NSNumber *value2 = [array objectAtIndex:j];
+            
+            int someNumber = value1.integerValue - value2.integerValue;
+            if (someNumber < 0) {
+                someNumber = someNumber * (-1);
+            }
+            if (someNumber == number.integerValue) {
+                count += 1;
+        }
+            
+            }
+    }
+    return count;
 }
 
 @end
