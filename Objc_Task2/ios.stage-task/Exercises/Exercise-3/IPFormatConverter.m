af23120f10a9379f99e14c18c8638bedfd14d98d
Tatyana Luzanova <82838416+Tane4ek@users.noreply.github.com>
2021-05-16 01:19:14 +0500

    docs: add solution of exercise 3

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,48 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    NSMutableString *mySrting = [[NSMutableString alloc] init];
+    NSMutableArray *array = [numbersArray mutableCopy];
+    if ([array count] == 0) {
+        [mySrting appendString:@" "];
+        NSLog(@"countstring %@", mySrting);
+    }
+    if ([array count] != 0) {
+        while ([array count] < 4) {
+        [array addObject:@0];
+        }
+    }
+    NSLog(@"new array%@", array);
+    if ([array count] > 4) {
+        while ([array count] != 4) {
+            [array removeLastObject];
+        }
+    }
+    NSLog(@"new array%@", array);
+    int num;
+    for (NSNumber* number in array) {
+        num = number.intValue;
+        if (num < 0) {
+            return @"Negative numbers are not valid for input.";
+            break;
+        }
+        if (num > 255) {
+            return @"The numbers in the input array can be in the range from 0 to 255.";
+            break;
+        }
+        NSLog(@"num %d", num);
+        NSMutableString *str = [[NSMutableString alloc] initWithFormat:@"%d",num];
+        NSLog(@"str %@", str);
+        [str insertString:@"." atIndex:str.length];
+        NSLog(@" newstr %@", str);
+        [mySrting insertString:str atIndex:mySrting.length];
+        NSLog(@"mystring %@", mySrting);
+    }
+    NSLog(@"%@", mySrting);
+    [mySrting deleteCharactersInRange:NSMakeRange(mySrting.length -1, 1)];
+
+    
+    return mySrting;
 }
 
 @end
