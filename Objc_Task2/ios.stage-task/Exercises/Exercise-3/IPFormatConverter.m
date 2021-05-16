04f6c6c24914c000dc3527474afec5374cec642a
Tatyana Luzanova <82838416+Tane4ek@users.noreply.github.com>
2021-05-17 01:03:24 +0500

    docs: add solution of exercise 4 and remove some code in solution of exercise 3

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -7,20 +7,17 @@
     NSMutableArray *array = [numbersArray mutableCopy];
     if ([array count] == 0) {
         [mySrting appendString:@" "];
-        NSLog(@"countstring %@", mySrting);
     }
     if ([array count] != 0) {
         while ([array count] < 4) {
         [array addObject:@0];
         }
     }
-    NSLog(@"new array%@", array);
     if ([array count] > 4) {
         while ([array count] != 4) {
             [array removeLastObject];
         }
     }
-    NSLog(@"new array%@", array);
     int num;
     for (NSNumber* number in array) {
         num = number.intValue;
@@ -32,18 +29,12 @@
             return @"The numbers in the input array can be in the range from 0 to 255.";
             break;
         }
-        NSLog(@"num %d", num);
         NSMutableString *str = [[NSMutableString alloc] initWithFormat:@"%d",num];
-        NSLog(@"str %@", str);
         [str insertString:@"." atIndex:str.length];
-        NSLog(@" newstr %@", str);
         [mySrting insertString:str atIndex:mySrting.length];
-        NSLog(@"mystring %@", mySrting);
     }
-    NSLog(@"%@", mySrting);
     [mySrting deleteCharactersInRange:NSMakeRange(mySrting.length -1, 1)];
 
-    
     return mySrting;
 }
 
--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,70 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    
+    NSMutableSet *setOfStrings = [[NSMutableSet alloc] init];
+    NSMutableSet *setOfNumbers = [[NSMutableSet alloc] init];
+    
+    for (int i = 0; i < [array count]; ++i) {
+        NSArray *subArray = array[i];
+        if (![subArray isKindOfClass:[NSArray class]]) {
+            return @[];
+        }
+        BOOL hasNumbers = NO;
+        BOOL hasStrings = NO;
+        for (int j = 0; j < [subArray count]; ++j) {
+            if ([subArray[j] isKindOfClass: [NSNumber class]]) {
+                [setOfNumbers addObject:subArray[j]];
+                hasNumbers = YES;
+            } else {
+                [setOfStrings addObject:subArray[j]];
+                hasStrings = YES;
+            }
+        }
+        if ((hasNumbers == YES) && (hasStrings == YES)) {
+            return @[];
+        }
+        
+    }
+    
+    NSMutableArray *arrayOfNumbers = [NSMutableArray arrayWithArray:[setOfNumbers allObjects]];
+    NSMutableArray *arrayOfStrings = [NSMutableArray arrayWithArray:[setOfStrings allObjects]];
+    [arrayOfNumbers sortUsingComparator: ^(id obj1, id obj2) {
+            if ([obj1 integerValue] > [obj2 integerValue]) {
+                return (NSComparisonResult)NSOrderedDescending;
+            }
+         
+            if ([obj1 integerValue] < [obj2 integerValue]) {
+                return (NSComparisonResult)NSOrderedAscending;
+            }
+            return (NSComparisonResult)NSOrderedSame;
+        }];
+    
+    NSArray *outputArray;
+    
+    if ([arrayOfStrings count] == 0) {
+        outputArray = arrayOfNumbers;
+    } else if ([arrayOfNumbers count] == 0) {
+        [arrayOfStrings sortUsingComparator: ^(id obj1, id obj2) {
+            NSComparisonResult res = [(NSString *)obj1 localizedCaseInsensitiveCompare:(NSString *)obj2];
+            
+            return res;
+        }];
+        outputArray = arrayOfStrings;
+        
+    } else {
+        [arrayOfStrings sortUsingComparator: ^(id obj1, id obj2) {
+            NSComparisonResult res = [(NSString *)obj1 localizedCaseInsensitiveCompare:(NSString *)obj2];
+        if (res == NSOrderedAscending)
+        res = NSOrderedDescending;
+        else if (res == NSOrderedDescending)
+            res = NSOrderedAscending;
+
+            return res;
+        }];
+        outputArray = @[arrayOfNumbers, arrayOfStrings];
+    }
+    return outputArray;
 }
 
 @end
